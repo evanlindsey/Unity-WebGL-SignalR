f3001b1e8a121085bbdcad78befcd6a62a4e0bdd
Mateus Faria <webmat1@hotmail.com>
2021-02-11 17:00:27 -0300

    Converting String to our Custom Class in C#

--- Server/Hubs/MainHub.cs
+++ Server/Hubs/MainHub.cs
@@ -1,16 +1,28 @@
 ﻿using Microsoft.AspNetCore.SignalR;
 using System.Text.Json;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Linq;
+using System;
 
 namespace SignalRServer.Hubs
 {
     public class MainHub : Hub
     {
+        public override Task OnConnectedAsync()
+        {
+            Console.WriteLine(Context.ConnectionId);
+            return base.OnConnectedAsync();
+        }
 
         public async Task SendPayloadA(string payload)
         {
-            var data = JsonSerializer.Deserialize<dynamic>(payload);
-            string json = JsonSerializer.Serialize(data);
+            var msg = "We are in A...";
+            Console.WriteLine(msg);
+            List<object> objects = new List<object>(){msg};
+            string json = JsonSerializer.Serialize(objects);
+            // var data = JsonSerializer.Deserialize<dynamic>(payload);
+            // string json = JsonSerializer.Serialize(data);
             await Clients.All.SendAsync("ReceivePayloadA", json);
         }
 
