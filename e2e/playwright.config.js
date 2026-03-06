978d998064881c5ed81bf9c33d5edee4a671c698
Evan Lindsey <evan@evanlindsey.net>
2026-03-05 20:10:18 -0600

    chore: playwright report

--- e2e/playwright.config.js
+++ e2e/playwright.config.js
@@ -4,9 +4,14 @@ module.exports = defineConfig({
   testDir: '.',
   timeout: 120_000,
   retries: 1,
+  outputDir: 'test-results',
+  reporter: process.env.CI
+    ? [['html', { open: 'never' }], ['github']]
+    : [['list', { printSteps: true }]],
   use: {
     headless: true,
     viewport: { width: 1024, height: 768 },
+    trace: 'retain-on-failure',
     launchOptions: {
       args: ['--use-gl=angle'],
     },
